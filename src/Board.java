34db38ca93f4be9fde7939deb94894a63ebb0f0f
ElDavido81 <eldavido81@gmail.com>
2024-10-22 16:37:09 +0200

    added random bomb placement

--- src/Board.java
+++ src/Board.java
@@ -2,130 +2,41 @@ import java.util.ArrayList;
 
 public class Board {
 
+    int cols = 6;
+    int rows = 6;
+    int numberOfBombs = 5;
+    int cellValue = 0;
+    int randomNum = 0;
+    int bombLoop = 0;
+
     public Board() {
     }
 
-    private String a1 = " ";
-    private String a2 = " ";
-    private String a3 = " ";
-    private String a4 = " ";
-    private String a5 = " ";
-    private String a6 = " ";
+    ArrayList<String> cells = new ArrayList<>();
 
-    private String b1 = " ";
-    private String b2 = " ";
-    private String b3 = " ";
-    private String b4 = " ";
-    private String b5 = " ";
-    private String b6 = " ";
-
-    private String c1 = " ";
-    private String c2 = " ";
-    private String c3 = " ";
-    private String c4 = " ";
-    private String c5 = " ";
-    private String c6 = " ";
-
-    private String d1 = " ";
-    private String d2 = " ";
-    private String d3 = " ";
-    private String d4 = " ";
-    private String d5 = " ";
-    private String d6 = " ";
-
-    private String e1 = " ";
-    private String e2 = " ";
-    private String e3 = " ";
-    private String e4 = " ";
-    private String e5 = " ";
-    private String e6 = " ";
-
-    private String f1 = " ";
-    private String f2 = " ";
-    private String f3 = " ";
-    private String f4 = " ";
-    private String f5 = " ";
-    private String f6 = " ";
-
-    public void displayEmptyBoard() {
-        System.out.println("   +–––+–––+–––+–––+–––+–––+");
-        System.out.println(" a | " + a1 + " | " + a2 + " | " + a3 + " | " + a4 + " | " + a5 + " | " + a6 + " |");
-        System.out.println("   +–––+–––+–––+–––+–––+–––+");
-        System.out.println(" b | " + b1 + " | " + b2 + " | " + b3 + " | " + b4 + " | " + b5 + " | " + b6 + " |");
-        System.out.println("   +–––+–––+–––+–––+–––+–––+");
-        System.out.println(" c | " + c1 + " | " + c2 + " | " + c3 + " | " + c4 + " | " + c5 + " | " + c6 + " |");
-        System.out.println("   +–––+–––+–––+–––+–––+–––+");
-        System.out.println(" d | " + d1 + " | " + d2 + " | " + d3 + " | " + d4 + " | " + d5 + " | " + d6 + " |");
-        System.out.println("   +–––+–––+–––+–––+–––+–––+");
-        System.out.println(" e | " + e1 + " | " + e2 + " | " + e3 + " | " + e4 + " | " + e5 + " | " + e6 + " |");
-        System.out.println("   +–––+–––+–––+–––+–––+–––+");
-        System.out.println(" f | " + f1 + " | " + f2 + " | " + f3 + " | " + f4 + " | " + f5 + " | " + f6 + " |");
-        System.out.println("   +–––+–––+–––+–––+–––+–––+");
-    }
-
-    public void displayBoard() {
-
-        int cols = 6;
-        int rows = 6;
-        int numberOfBombs = 5;
-        int cellValue = 0;
-
-        ArrayList<String> cells = new ArrayList<>();
+    public void createBoard() {
 
         System.out.println("     a   b   c   d   e   f");
-        System.out.println("   +–––+–––+–––+–––+–––+–––+");
 
         for (int i = 0; i < (rows); i++) {
             System.out.print((i + 1) + "  |");
 
             for (int j = 0; j < (cols); j++) {
-                cells.add(String.valueOf(cellValue));
-
+            cells.add("C");
                 System.out.print(" " + cells.get(cellValue) + " |");
-//                int randomNum = (int) Math.floor(Math.random() * (rows * cols)) + 1;
-//                if (j == randomNum) {
-//                    cells.add(j, "B");
                 cellValue++;
             }
-            System.out.println("\n   +–––+–––+–––+–––+–––+–––+");
+            System.out.println();
+        }
     }
 
+        public void placeBombs() {
+            while (bombLoop < numberOfBombs) {
+                randomNum = (int) Math.floor(Math.random() * (rows * cols)) + 1;
+                cells.add(randomNum, "B");
+                System.out.println(randomNum);
+                bombLoop++;
+            }
+        }
 
-
-    }
 }
-
-
-
-//    }
-//
-//    public void placeBombs() {
-//        String bomb = "";
-//
-//        for (int i = 0; i < 5; i++) {
-//
-//            int randomNum = (int) Math.floor(Math.random() * 6) + 1;
-//            int randomChar = (int) Math.floor(Math.random() * 6) + 1;
-//
-//            if (randomChar == 1) {
-//                bomb = ("a" + randomNum);
-//            } else if (randomChar == 2) {
-//                bomb = ("b" + randomNum);
-//            } else if (randomChar == 3) {
-//                bomb = ("c" + randomNum);
-//            } else if (randomChar == 4) {
-//                bomb = ("d" + randomNum);
-//            } else if (randomChar == 5) {
-//                bomb = ("e" + randomNum);
-//            } else if (randomChar == 6) {
-//                bomb = ("f" + randomNum);
-//            }
-//
-//        }
-//    }
-//}
-//
-//
-//
-
-
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         Board myBoard = new Board();
-        myBoard.displayBoard();
+        myBoard.createBoard();
+        myBoard.placeBombs();
 //        myBoard.displayEmptyBoard();
 //        myBoard.placeBombs();
     }
