936f18b3efad5c49b8f28ea9d72db10c54597688
ElDavido81 <eldavido81@gmail.com>
2024-10-22 18:01:50 +0200

    double digit fix

--- src/Board.java
+++ src/Board.java
@@ -3,20 +3,25 @@ import java.util.List;
 
 public class Board {
 
-    int cols = 10;
-    int rows = 4;
-    int numberOfBombs = 3;
-    int cellValue = 0;
-    int randomNum = 0;
-    int bombLoop = 0;
+    int cols;
+    int rows;
+    int numberOfBombs;
+    int cellValue;
+    int randomNum;
+    int bombLoop;
 
-    public Board() {
+    public Board(int cols, int rows, int numberOfBombs) {
+    this.cols = cols;
+    this.rows = rows;
+    this.numberOfBombs = numberOfBombs;
+    this.cellValue = 0;
+    this.randomNum = 0;
+    this.bombLoop = 0;
     }
 
     ArrayList<String> cells = new ArrayList<>();
 
-    public void createBoard() {
-
+    public void createBoard() {         // Skapa ett tomt spelbräde
 
         for (int i = 0; i < (rows); i++) {
             System.out.print((i + 1) + "  |");
@@ -30,7 +35,7 @@ public class Board {
         }
     }
 
-        public void placeBombs() {
+        public void placeBombs() {          // Slumpa bombernas placering
             while (bombLoop < numberOfBombs) {
                 randomNum = (int) Math.floor(Math.random() * (rows * cols)) + 1;
                 cells.set(randomNum, "B");
@@ -39,7 +44,7 @@ public class Board {
             }
         }
 
-    public void displayBoard() {
+    public void displayBoard() {            // Visa spelplanen
         cellValue = 0;
 
         int length = cols;
@@ -47,19 +52,26 @@ public class Board {
         for (int i = 0; i < length; i++) {
             alphabetList.add((char)('a' + i));
         }
-        System.out.print("     ");
+        System.out.print("      ");
         for (Character c : alphabetList) {
         System.out.print(c + "   ");
         }
         System.out.println();
 
         for (int i = 0; i < (rows); i++) {
-            System.out.print((i + 1) + "  |");
+
+            String space = "";      // Knuffar tvåsiffriga tal ett steg åt höger
+            if (i < 9){             //
+                space = " ";        //
+            }
+
+            System.out.print(space + (i + 1) + "  |");
 
             for (int j = 0; j < (cols); j++) {
                 System.out.print(" " + cells.get(cellValue) + " |");
                 cellValue++;
             }
+
             System.out.println();
         }
     }
