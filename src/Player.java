f241f1093c069206e93dffef31de7ccedfe33b28
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-10-22 09:55:32 +0200

    Added Player Class

---
+++ src/Player.java
@@ -0,0 +1,22 @@
+public class Player {
+    private final String name;
+    private int score;
+
+    // Const
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void addScore(int score) {
+        this.score++;
+    }
+}
