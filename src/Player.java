00c0486b3df0eba2f885d775f66963eee9db496e
beles <beles.dev@gmail.com>
2024-10-24 14:21:50 +0200

    Added some comments.

--- src/Player.java
+++ src/Player.java
@@ -3,15 +3,32 @@ public class Player {
     private char playerSymbol;
 
     // Const
-    public Player(String name) {
+    public Player(String name, char playerSymbol) {
         this.name = name;
         this.playerSymbol = playerSymbol;
     }
 
+
+    /**
+     *
+     * @return Return the player name.
+     */
     public String getName() {
         return name;
     }
+
+
+    /**
+     *
+     * @return Sets the player symbol.
+     */
     public char setPlayerSymbol() {return playerSymbol;}
+
+
+    /**
+     *
+     * @return Return the player char symbol
+     */
     public char getPlayerSymbol() {return playerSymbol;}
 
 
