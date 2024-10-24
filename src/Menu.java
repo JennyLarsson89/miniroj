17993e939a0248e6725347a368f00b4c5e1f1a88
ElDavido81 <eldavido81@gmail.com>
2024-10-24 10:50:43 +0200

    created menu-class

---
+++ src/Menu.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Menu {
+
+    Scanner sc = new Scanner(System.in);
+
+    public void menu() {
+        while (true) {
+            try {
+
+                System.out.println("Welcome to minesweeper! Whats your name?");
+                System.out.println("Hello, " + playerName.getName + " press 1 to Play or 2 to Quit");
+                String playerName = sc.nextLine();
+
+                int menuChoice = sc.nextInt();
+
+                if (menuChoice == 1){
+
+                } else if (menuChoice == 2){
+                    System.exit(2);
+                }
+
+
+            } catch (Exception e){
+                System.out.println("Invalid input");
+
+            }
+        }
+        }
+
+    }
