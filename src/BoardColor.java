7384a7d9aef6235b1030649cda88f6766ce314db
beles <beles.dev@gmail.com>
2024-10-24 19:29:17 +0200

    Created the BoardColor Class to give the board a little color.

---
+++ src/BoardColor.java
@@ -0,0 +1,15 @@
+
+public class BoardColor {
+
+    /**
+     * Color for the board
+     */
+    public String ANSI_RESET = "\u001B[0m";
+    public String ANSI_RED = "\u001B[31m";
+    public String ANSI_GREEN = "\u001B[92m";
+    public String ANSI_YELLOW = "\u001B[93m";
+    public String ANSI_BLUE = "\u001B[34m";
+    public String ANSI_CYAN = "\u001B[36m";
+
+
+}
