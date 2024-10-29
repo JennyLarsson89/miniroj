dde9619fcf3c31dbcfa32d26980ae6a07f606f60
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-10-29 10:32:58 +0100

    little changes

--- src/Game.java
+++ src/Game.java
@@ -1,28 +1,17 @@
-import java.util.Scanner;
 
 public class Game {
 
-    Scanner sc = new Scanner(System.in);
     Board board = new Board(6, 6);
     boolean gameOver = false;
 
     /**The game loop starts here.*/
-    public void play(){
+    public void play() {
         board.initializeBoard();
-        board.displayBoard();
         board.placeBombs();
-        while(!gameOver){
-        board.selectCell();
-        board.displayBoard();
-        }
-        board.finalBoard();
-    }
 
-    public boolean checkDeath(int playerRow, int playerCol, String[][] board) {
-        if (board[playerRow][playerCol].equals("o")) {
-            System.out.println("Death");
-            return true;
+        while(!gameOver) {
+            board.selectCell();
+            gameOver = true;
         }
-        return false;
     }
 }
