7d632afccd3e545f7f125867ef4824c3e1ab9c36
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-10-29 10:30:58 +0100

    Rename

---
+++ src/Color.java
@@ -0,0 +1,29 @@
+public class Color {
+    // ANSI escape codes for colors
+    public static final String RESET = "\033[0m";  // Reset to default color
+    public static final String RED = "\033[0;31m";
+    public static final String GREEN = "\033[0;32m";
+    public static final String YELLOW = "\033[0;33m";
+    public static final String BLUE = "\033[0;34m";
+    public static final String PURPLE = "\033[0;35m";
+    public static final String CYAN = "\033[0;36m";
+    public static final String WHITE = "\033[0;37m";
+    public static final String BRT_BLACK = "\033[1;30m";
+    public static final String BRT_RED = "\033[1;31m";
+    public static final String BRT_GREEN = "\033[1;32m";
+    public static final String BRT_YELLOW = "\033[1;33m";
+    public static final String BRT_BLUE = "\033[1;34m";
+    public static final String BRT_MAGENTA = "\033[1;35m";
+    public static final String BRT_PURPLE = "\033[1;35m";
+    public static final String BRT_CYAN = "\033[1;36m";
+    public static final String BRT_WHITE = "\033[1;37m";
+    public static final String BG_BLACK = "\033[40m";
+    public static final String BG_RED = "\033[41m";
+    public static final String BG_GREEN = "\033[42m";
+    public static final String BG_YELLOW = "\033[43m";
+    public static final String BG_BLUE = "\033[44m";
+    public static final String BG_MAGENTA = "\033[45m";
+    public static final String BG_PURPLE = "\033[45m";
+    public static final String BG_CYAN = "\033[46m";
+    public static final String BG_WHITE = "\033[47m";
+}
