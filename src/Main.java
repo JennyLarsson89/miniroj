4b9ec5e380696172b3e82bc583037e20e622fb59
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-10-31 18:36:56 +0100

    Added do while loop

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
 
-       Menu menu = new Menu();
-       menu.runMenu();
+        Scanner sc = new Scanner(System.in);
+        boolean playAgain;
 
+        do {
+            Game game = new Game();
+            game.start();
+
+            System.out.print("Vill du spela igen? (j/n): ");
+            playAgain = sc.next().equals("j");
+
+        } while (playAgain);
+
+        System.out.println("Tack för att du spelade!");
+        sc.close();
     }
 }
