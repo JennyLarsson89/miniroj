60f823298c03c64ffa0bdad840ff8ddae0e42ab9
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-10-31 18:35:01 +0100

    Added Color class

---
+++ src/Color.java
@@ -0,0 +1,56 @@
+public class Color {
+    // ANSI escape codes for colors
+    public static final String RESET = "\033[0m";  // Reset to default color
+    public static final String RED = "\033[0;31m";
+    public static final String GREEN = "\033[0;32m";
+    public static final String YELLOW = "\033[0;33m";
+    public static final String BLUE = "\033[0;34m";
+    public static final String PURPLE = "\033[0;35m";
+    public static final String CYAN = "\033[0;36m";
+    public static final String WHITE = "\033[0;37m";
+    public static final String BRT_BLACK = "\033[1;30m";
+    public static final String BRT_RED = "\033[1;31m";
+    public static final String BRT_GREEN = "\033[1;32m";
+    public static final String BRT_YELLOW = "\033[1;33m";
+    public static final String BRT_BLUE = "\033[1;34m";
+    public static final String BRT_MAGENTA = "\033[1;35m";
+    public static final String BRT_PURPLE = "\033[1;35m";
+    public static final String BRT_CYAN = "\033[1;36m";
+    public static final String BRT_WHITE = "\033[1;37m";
+    public static final String BG_BLACK = "\033[40m";
+    public static final String BG_RED = "\033[41m";
+    public static final String BG_GREEN = "\033[42m";
+    public static final String BG_YELLOW = "\033[43m";
+    public static final String BG_BLUE = "\033[44m";
+    public static final String BG_MAGENTA = "\033[45m";
+    public static final String BG_PURPLE = "\033[45m";
+    public static final String BG_CYAN = "\033[46m";
+    public static final String BG_WHITE = "\033[47m";
+}
+
+//    public static void main(String[] args) {
+//
+//        System.out.println(ANSI_RED + "This text is red!" + RESET);
+//        System.out.println(ANSI_WHITE_BG + "This text is red!" + RESET);
+//        System.out.println(ANSI_BLACK_BG + "This text is red!" + RESET);
+//        System.out.println(RED + "This text is red!" + RESET);
+//        System.out.println(GREEN + "This text is green!" + RESET);
+//        System.out.println(YELLOW + "This text is yellow!" + RESET);
+//        System.out.println(BLUE + "This text is blue!" + RESET);
+//        System.out.println(PURPLE + "This text is purple!" + RESET);
+//        System.out.println(CYAN + "This text is cyan!" + RESET);
+//        System.out.println(BRIGHT_RED + "This text is bright red!" + RESET);
+//        System.out.println(BRIGHT_GREEN + "This text is bright green!" + RESET);
+//        System.out.println(BRIGHT_YELLOW + "This text is bright yellow!" + RESET);
+//        System.out.println(BRIGHT_BLUE + "This text is bright blue!" + RESET);
+//        System.out.println(BRIGHT_PURPLE + "This text is bright purple!" + RESET);
+//        System.out.println(BRIGHT_CYAN + "This text is bright cyan!" + RESET);
+//        System.out.println(BACKGROUND_RED + "This text has a red BG!" + RESET);
+//        System.out.println(BACKGROUND_GREEN + "This text has a green BG!" + RESET);
+//        System.out.println(BACKGROUND_YELLOW + "This text has a yellow background!" + RESET);
+//        System.out.println(BACKGROUND_BLUE + "This text has a blue background!" + RESET);
+//        System.out.println(BACKGROUND_PURPLE + "This text has a purple background!" + RESET);
+//        System.out.println(BACKGROUND_CYAN + "This text has a cyan background!" + RESET);
+//        System.out.println(BACKGROUND_WHITE + "This text has a WHITE background!" + RESET);
+//    }
+
